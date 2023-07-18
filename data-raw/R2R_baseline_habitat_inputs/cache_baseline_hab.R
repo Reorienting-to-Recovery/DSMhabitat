4fa863e4d1b0ecbefdf3d1e625931c02c3b73654
Maddee Rubenson <mrubenson@flowwest.com>
2023-07-18 08:55:33 -0700

    adding max flow with hab projects into delta_hab

--- data-raw/R2R_baseline_habitat_inputs/cache_baseline_hab.R
+++ data-raw/R2R_baseline_habitat_inputs/cache_baseline_hab.R
@@ -809,5 +809,6 @@ delta |>
   theme_minimal()
 
 # Save as data object to DSMhabitat
-delta_habitat <- c(sit_habitat = list(DSMhabitat::delta_habitat$sit_habitat), r_to_r_baseline = list(r_to_r_baseline_delta))
+delta_habitat = modifyList(DSMhabitat::delta_habitat, list(sit_habitat = DSMhabitat::delta_habitat$sit_habitat,
+                                                           r_to_r_baseline = r_to_r_baseline_delta))
 usethis::use_data(delta_habitat, overwrite = TRUE)
