b8183397ec10971b8a12b93dc324e48d50c5ac53
liz-fw <lstebbins@flowwest.com>
2025-02-25 11:46:13 -0800

    removes cache_eff_with_sj file because it is redundant and exactly the same as the cache_eff file

--- data-raw/update_data.R
+++ data-raw/update_data.R
@@ -5,7 +5,6 @@ source("data-raw/cached-habitat.R")
 # R2R baseline scaling on various hydrologies
 source("data-raw/R2R_baseline_habitat_inputs/cache_baseline_hab.R")
 source("data-raw/R2R_baseline_habitat_inputs/cache_eff_with_baseline_hab.R")
-source("data-raw/R2R_baseline_habitat_inputs/cache_eff_sj_with_baseline_hab.R") # TODO not sure this does anything
 source("data-raw/R2R_baseline_habitat_inputs/cache_max_flow_with_added_habitat_project.R")
 source('data-raw/R2R_baseline_habitat_inputs/cache_lto_12a_with_baseline_hab.R')
 
