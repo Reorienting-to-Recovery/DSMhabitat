fbd3a9f163f957be0d00f4aa3bce1a4879c86003
liz-fw <lstebbins@flowwest.com>
2026-02-25 16:42:08 -0800

    adds scripts to update-data.R sourcing and builds data objects

--- data-raw/update_data.R
+++ data-raw/update_data.R
@@ -1,12 +1,12 @@
 # run this script to update all the habitat data
 # Base data
-source("data-raw/cached-habitat.R") # TODO add action 5 baseline
+source("data-raw/cached-habitat.R")
 
 # R2R baseline scaling on various hydrologies
 source("data-raw/R2R_baseline_habitat_inputs/cache_baseline_hab.R")
-# TODO add script for caching baseline with action 5 -- see cache_lto_12a_with_baseline_hab.R
+source("data-raw/R2R_baseline_habitat_inputs/cache_action5_with_baseline_hab.R")
 
 # Max Habitat scaling on various hydrologies
 source("data-raw/R2R_TMH_habitat_inputs/cache_tmh_data.R")
 source("data-raw/R2R_TMH_habitat_inputs/cache_run_of_river_tmh_data.R")
-# TODO add script for caching tmh with action 5 (upper sacramento only)
+source("data-raw/above_shasta_habitat/cache_action_5_tmh_data.R")
